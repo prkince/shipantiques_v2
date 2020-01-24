2fce8f9354059c2ba2c1831ad5576492f011db43
Pierrick Delage <pierrick.delage@gmail.com>
2020-01-24 15:35:03 +0100

    website done with most sections

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,15 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home]
+  skip_before_action :authenticate_user!, only: [:home, :services, :specialites, :contact]
 
   def home
   end
+
+  def services
+  end 
+
+  def specialites
+  end
+
+  def contact
+  end  
 end
