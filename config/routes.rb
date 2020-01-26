13ad8c18b831e4a67cbbd31618868a590de353f7
Pierrick Delage <pierrick.delage@gmail.com>
2020-01-26 16:39:53 +0100

    annonces upgraded

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get 'users/annonces'
+  post 'users/annonces', to: 'users#annonces'
+
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
