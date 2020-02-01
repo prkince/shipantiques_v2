bd705e77e228fb0976d22d2f3c8ec7c46a2adf57
Pierrick Delage <pierrick.delage@gmail.com>
2020-02-01 17:49:16 +0100

    working on the form, files are not sent as attachment

--- app/controllers/cotations_controller.rb
+++ app/controllers/cotations_controller.rb
@@ -8,18 +8,19 @@ class CotationsController < ApplicationController
 	def create
 		@cotation = Cotation.new(params[:cotation])
 		@cotation.request = request
+		@cotation.oeuvres = params[:cotation][:oeuvres][0]
 		if @cotation.deliver
-		  flash.now[:notice] = 'Thank you for your message. We will contact you soon!'
+		  flash.now[:notice] = 'Votre demande de cotation à bien été transmise, nous allons revenir vers vous au plus vite'
 		else
-		  flash.now[:error] = 'Cannot send message.'
-		  #render :new
+		  flash.now[:error] = 'Merci de vérifier votre saisie svp'
+		  render :new
 		end
 	end
 
 	private
 
 	def contact_params
-		params.require(:cotation).permit(:email, :name, :message)
+		params.require(:cotation).permit(:email, :name, :company, :tel, :message, :choices, :oeuvres, :type, :address_enl, :address_liv, :description_oeuvres, :description_prestation, :date_depart, :date_fin)
 	end  
 
 end
--- app/models/cotation.rb
+++ app/models/cotation.rb
@@ -1,15 +1,27 @@
 class Cotation < MailForm::Base
 
+	CHOICES = ["Musée / Institution",  "Galerie", "Commissaire Priseur", "Artiste", "Particulier", "Autre"]
+	DEMANDES = ["Exposition", "Manutention", "Stockage", "Transport", "Déménagement", "Autre"]
 	attribute :name,      :validate => true
 	attribute :email,     :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
-	attribute :message
+	attribute :company
+	attribute :tel
+	attribute :choices
+	attribute :oeuvres
+	attribute :type
+	attribute :address_enl
+	attribute :address_liv
+	attribute :description_oeuvres
+	attribute :description_prestation
+	attribute :date_depart
+	attribute :date_fin
 	attribute :nickname,  :captcha  => true
 
 	# Declare the e-mail headers. It accepts anything the mail method
 	# in ActionMailer accepts.
 	def headers
 	{
-	  :subject => "My Contact Form",
+	  :subject => "Demande de cotation via le site Shipantiques.fr",
 	  :to => ENV["GMAIL_USERNAME"],
 	  :from => %("#{name}" <#{email}>)
 	}
