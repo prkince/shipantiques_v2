c0b0dd1976acbaade09f936b4d2c1fefb56c7281
Pierrick Delage <pierrick.delage@gmail.com>
2020-02-12 11:26:58 +0100

    doing some JS for anchors

---
+++ app/javascript/components/anchor.js
@@ -0,0 +1,11 @@
+
+// The function actually applying the offset
+function offsetAnchor() {
+  if(location.hash.length !== 0) {
+    window.scrollTo(window.scrollX, window.scrollY - 100);
+  }
+}
+window.addEventListener("hashchange", offsetAnchor);
+window.setTimeout(offsetAnchor, 1);
+
+export { offsetAnchor };
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,6 +11,9 @@ import "bootstrap";
 
 // *** components ***
 
+import { offsetAnchor } from '../components/anchor';
+offsetAnchor();
+
 AOS.init({
   duration: 800,
   easing: 'slide',
@@ -168,3 +171,4 @@ jQuery(document).ready(function($) {
   OnePageNavigation();
 
 });
+
